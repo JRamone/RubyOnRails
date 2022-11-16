5f6cc3a0064cf7a3bef22fb8961089ff680f747b
Jore <JRamone>
2022-11-16 10:17:02 +0200

    add week2 exercise14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -11,4 +12,9 @@ class Brewery < ApplicationRecord
     self.year = 2022
     puts "changed year to #{year}"
   end
+
+  def average_rating
+    self.ratings.average(:score)  
+  end
 end
+
