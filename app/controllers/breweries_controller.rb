a03962645b49f5523db1af56cf6655d45f6cca9b
Jore <JRamone>
2022-11-16 13:34:49 +0200

    add week2

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,7 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: %i[ show edit update destroy ]
-
+  before_action :authenticate, only: [:destroy]
+    
   # GET /breweries or /breweries.json
   def index
     @breweries = Brewery.all
@@ -61,6 +62,14 @@ class BreweriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_brewery
       @brewery = Brewery.find(params[:id])
+    end
+
+    def authenticate
+      admin_accounts = { "pekka" => "beer", "arto" => "foobar", "matti" => "ittam", "vilma" => "kangas"}
+      authenticate_or_request_with_http_basic do |username, password|
+        raise "Wrong username or password" unless password == admin_accounts[username]
+        return true
+      end
     end
 
     # Only allow a list of trusted parameters through.
