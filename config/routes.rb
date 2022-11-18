070b8af6778303c3b6f54b2f2a98f54259a234dc
Jore <JRamone>
2022-11-18 16:55:43 +0200

    Add hours

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   root 'breweries#index'
   get 'kaikki_bisset', to: 'beers#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ hours.md
@@ -0,0 +1,3 @@
+| Day       | Hours| Description  |
+| :-------: |:----:| :-----------:|
+| 18.11.2022| 2h   | How to keep track of time, continue on week 3 |
