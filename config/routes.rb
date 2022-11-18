56117df531df4151dff0f6da0b0cbdf0120c5797
Jore <JRamone>
2022-11-18 18:34:17 +0200

    add week3 exercise3

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'kaikki_bisset', to: 'beers#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
+  resource :session, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
