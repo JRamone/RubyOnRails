9a372e78b98b5ca8fd9e1a694377fe9f044a8378
Jore <JRamone>
2022-11-19 10:23:10 +0200

    add week3 exercise6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,7 +10,8 @@ class RatingsController < ApplicationController
 
   def create
     rating =  Rating.create params.require(:rating).permit(:score, :beer_id)
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
+    rating.user = current_user
+    rating.save
     redirect_to ratings_path
   end
 
