4bcc7fc00dddbe16d81728e4b26c6cc7f2ef91b5
Jore <JRamone>
2022-11-19 08:14:32 +0200

    add week3 exercise4

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
   resource :session, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
