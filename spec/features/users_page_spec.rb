b86e731b31b52177b29fbd4887beff8fc1b19bb7
Jore <JRamone>
2022-12-07 15:33:36 +0200

    fix rating deletion problem

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -87,7 +87,7 @@ describe "User" do
         create_beer_with_rating({user: user})
         rating_to_be_deleted = create_beer_with_rating({user: user})
         visit user_path(user)
-
+        puts page.html
         expect(page).to have_content("Has made #{Rating.count} ratings,")
         expect(Rating.count).to eq(2)
 
