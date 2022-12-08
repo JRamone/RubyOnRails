b01ff9b80e03dff6d13b36a2b1ed7b98302e3b40
Jore <JRamone>
2022-12-08 16:04:03 +0200

    add beermapping search

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,14 @@ class PlacesController < ApplicationController
   end
 
   def search
-    render :index
+    api_key = "a25d9f2f0098df836513c302dd16daca"
+    url = "http://beermapping.com/webservice/loccity/#{api_key}/"
+
+    response = HTTParty.get "#{url}#{params[:city]}"
+    @places  = response.parsed_response["bmp_locations"]["location"].map do |place|
+      Place.new(place)
+    end
+
+    render :index, status: 418
   end
 end
-
