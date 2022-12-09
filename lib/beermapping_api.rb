f74d049dbaa122d6a6db8d2b33bcd36b785722ce
JRamone <jmkoho@gmail.com>
2022-12-09 16:57:30 +0200

    Add cache lifetime

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,7 +1,7 @@
 class BeermappingApi
   def self.places_in(city)
     city = city.downcase
-    Rails.cache.fetch(city) { get_places_in(city) }
+    Rails.cache.fetch(city,expires_in: 1.minute) { get_places_in(city) }
   end
 
   def self.get_places_in(city)
@@ -10,7 +10,7 @@ class BeermappingApi
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places.is_a?(Hash) && places['id'].nil?
+    return [] if places.is_a?(Hash) and places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
     places.map do |place|
