ffbee088101da7ca71e3fb898af13232b9aa27ef
Jore Matias Koho <36995587+JRamone@users.noreply.github.com>
2022-12-11 20:43:13 +0200

    Update 20221210171351_remove_style_from_beer.rb

--- db/migrate/20221210171351_remove_style_from_beer.rb
+++ db/migrate/20221210171351_remove_style_from_beer.rb
@@ -1,7 +1,5 @@
 class RemoveStyleFromBeer < ActiveRecord::Migration[7.0]
   def change
-    change_table :beers do |b|
-      b.remove :style, :string
-    end
+      remove_column :beers, :style, :string
   end
 end
